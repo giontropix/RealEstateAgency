2ba4b6f2795de9a8cdc5c27f788efcc3a4552b10
giontropix <giontropix@msn.com>
2020-05-22 10:46:25 +0200

    cleaned up code

--- src/House.java
+++ src/House.java
@@ -5,27 +5,31 @@ public class House implements Serializable {
     int surface;
     int distanceFromSea;
     int price;
+
     public House(String name, int surface, int distanceFromSea) {
         this.name = name;
         this.surface = surface;
         this.distanceFromSea = distanceFromSea;
     }
+
     public String getName() {
         return name;
     }
+
     public int getSurface() {
         return surface;
     }
+
     public int getDistanceFromSea() {
         return distanceFromSea;
     }
+
     public int setPrice() {
         if(getDistanceFromSea() <= 200)
-            price = (getSurface() * 2000);
+            return (getSurface() * 2000);
         else if(getDistanceFromSea() <= 1000)
-            price = (getSurface() * 1500);
+            return (getSurface() * 1500);
         else
-            price = (getSurface() * 1000);
-        return price;
+            return (getSurface() * 1000);
     }
 }
